b9c945ff274272d00c2b35fef0a5e80badb075b3
Shadow_Fox_x <ShallyDragon@gmail.com>
2025-06-12 15:39:16 +0300

    изменили порты

--- 15-01.js
+++ 15-01.js
@@ -169,7 +169,7 @@ app.get('/', async function(req, res) {
 	});
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
